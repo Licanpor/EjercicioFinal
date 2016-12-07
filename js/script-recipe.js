20f877e0e97cfe2ecbe54f48f784d8bb0c3076a3
LilianaCanPor <liliana.canpor@gmail.com>
2016-12-07 10:06:31 -0600

    Etapa 8 y 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,5 +2,23 @@ $(document).ready( function(){
 console.log('Este era una gato que estaba cenando cuando se le apago su candil...');
   $('.js-menu').hide();
   $('.js-back').show();
+
+  $('.js-show-recipe').click(function(){
+  	$('.page').removeClass('make');
+  	$('.js-show-recipe').addClass('active');
+  	$('.js-show-make').removeClass('active');
+
+  });
+
+  $('.js-show-make').click(function(){
+  	$('.page').addClass('make');
+  	$('.js-show-make').addClass('active');
+  	$('.js-show-recipe').removeClass('active');
+
+  });
+
+
+
  
 });
+
