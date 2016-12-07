66ca45ee3797c2c1c0e7a0e5054cd3b50ad3c03a
LilianaCanPor <liliana.canpor@gmail.com>
2016-12-06 19:21:17 -0600

    etapa 5

--- js/script-index.js
+++ js/script-index.js
@@ -10,6 +10,8 @@ $(document).ready( function(){
 	};
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	renderActivities(activities);
+
 
 });
 
@@ -86,8 +88,19 @@ function renderRecipe(recipe) {
 
 /** Función que se encarga de pintar todas las actividades
 */
-function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+function renderActivities(activities) {
+	console.log('Activities: ', activities);
+	_.each(activities, function(){
+		renderActivity(activities);
+		console.log('HOLA');
+
+		if (activities){	
+			if(activities != 0) {
+				$('.wrapper-message').hide();
+				console.log('YUPI');
+			}
+		}	
+	});
 }
 
 /*
